c00c04502755976cce9f4647bd7a546708b17942
bielbielbielbiel <carvalhogabriel75@gmail.com>
2025-11-20 19:49:08 -0300

    Comentários

--- servidor.py
+++ servidor.py
@@ -1,3 +1,7 @@
+# http://127.0.0.1:6789/HelloWorld.html
+# http://127.0.0.1:6789/nada.html
+
+
 # Importa o módulo socket 
 from socket import * 
 import sys  # Necessário para encerrar o programa 
@@ -6,31 +10,31 @@ import sys  # Necessário para encerrar o programa
 serverSocket = socket(AF_INET, SOCK_STREAM) 
 
 # Prepara o socket do servidor 
-#Fill in start 
-serverPort = 6789
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-#Fill in end 
+
+serverPort = 6789 # define o endereço IP e a porta do servidor 
+serverSocket.bind(('', serverPort)) # '' = aceita conexões de qualquer IP
+serverSocket.listen(1) # fica aguardando a conexão (Apenas uma)
+
 
 while True: 
     # Estabelece a conexão 
     print('Ready to serve...') 
-    connectionSocket, addr = serverSocket.accept()  #Fill in start     #Fill in end 
+    connectionSocket, addr = serverSocket.accept() # cria uma nova conexão e armazena o endereço do cliente
 
     try: 
         # Recebe a mensagem do cliente (requisição HTTP) 
-        message = connectionSocket.recv(1024).decode()  #Fill in start     #Fill in end 
+        message = connectionSocket.recv(1024).decode() # recebe até 1024 bytes / decode transforma em texto
 
-        filename = message.split()[1]   
-        f = open(filename[1:]) 
-        outputdata = f.read()  #Fill in start     #Fill in end 
+        filename = message.split()[1]   # separa a requisição em partes e pega o segundo item(?HelloWrold)
+        
+        f = open(filename[1:]) # abre o arquivo no computador
+        outputdata = f.read() # lê o HTML 
 
-        # Envia a linha de status do cabeçalho HTTP 
-        #Fill in start 
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-        #Fill in end 
+        # envia a linha de status do cabeçalho HTTP 
 
-        # Envia o conteúdo do arquivo ao cliente 
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode()) # Diz ao navegador qua a requisição foi bem sucedida / r,n separa o conteúdo
+
+        # Envia o conteúdo do arquivo ao cliente
         for i in range(0, len(outputdata)): 
             connectionSocket.send(outputdata[i].encode()) 
         connectionSocket.send("\r\n".encode()) 
@@ -40,15 +44,14 @@ while True:
 
     except IOError: 
         # Envia mensagem de erro 404 se o arquivo não for encontrado 
-        #Fill in start 
+        # Página HTML dizendo que o arquivo não existe
+    
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
-        #Fill in end 
 
         # Fecha o socket do cliente 
-        #Fill in start 
+
         connectionSocket.close()
-        #Fill in end 
 
     serverSocket.close() 
     sys.exit()  # Encerra o programa 
